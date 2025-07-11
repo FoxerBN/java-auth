baa22b567f695e75f41d41287a2232fc397c62e8
richardtekula <barspin4499@gmail.com>
2025-07-11 19:02:55 +0200

    sixth commit

--- src/main/java/sk/foxer/java_auth/config/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/sk/foxer/java_auth/config/OAuth2AuthenticationSuccessHandler.java
@@ -3,6 +3,7 @@ package sk.foxer.java_auth.config;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,16 @@ import java.io.IOException;
 @Component
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
+
     @Autowired
     private TokenProvider tokenProvider;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         String token = tokenProvider.createToken(authentication);
-        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:5173/profile")
+        String targetUrl = UriComponentsBuilder.fromUriString(frontendUrl+"/profile")
                 .queryParam("token", token)
                 .build().toUriString();
 
